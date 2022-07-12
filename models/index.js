9e2008ddd8997c8836325beb33031c26c0f1a74b
joechaaya <99142621+joechaaya@users.noreply.github.com>
2022-07-12 18:51:51 -0400

    Add files via upload

---
+++ models/index.js
@@ -0,0 +1,5 @@
+const Users = require('./Users');
+const Thought = require('./Thought');
+const models = require('./models')
+
+module.exports = { Users, Thought, models };
--- server.js
+++ server.js
@@ -18,4 +18,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Social-Ne
 
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
